ac86be83523afa1c72c6a3460c2316cda7b71665
Ransika <57299043+RansikaP@users.noreply.github.com>
2024-04-10 21:11:41 -0400

    uibuild

--- index.js
+++ index.js
@@ -65,12 +65,6 @@ app.delete('/api/persons/:id', (request, response, next) => {
 app.post('/api/persons', async (request, response, next) => {
     const body = request.body
 
-    if (!body.name || !body.number) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
-
     const person = new Person({
         name: body.name,
         number: body.number
@@ -84,7 +78,11 @@ app.post('/api/persons', async (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndUpdate({ _id: request.params.id}, request.body, { new: true })
+    Person.findByIdAndUpdate(
+        { _id: request.params.id}, 
+        request.body, 
+        { new: true, runValidators: true, context: 'query' }
+    )
         .then(updatedPerson => {
             response.json(updatedPerson)
         })
@@ -102,7 +100,11 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformed id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
+
+    next(error)
 }
 
 app.use(errorHandler)
